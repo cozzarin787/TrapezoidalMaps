e97eeca7351bf4d35809fc08d1e13be9467394c2
Jacob Cozzarin <cozzarin787@gmail.com>
2019-11-12 18:15:24 -0500

    Added python file to implement trapezoidal maps

---
+++ trapezoidal_map.py
@@ -0,0 +1,13 @@
+"""
+    File: trapezoidal_map.py
+    Authors: Jacob Cozzarin and <ADD YOUR NAME HERE>
+
+    Description: Trapezoidal map construction and planar point location implementation
+"""
+
+def main():
+    print("Got to start somewhere")
+
+
+if __name__ == "__main__":
+    main()
