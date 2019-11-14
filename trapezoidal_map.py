1f1bcad91e83e4ec31ddd6f45b8dd63181ce3ab5
William <willtc111@gmail.com>
2019-11-14 13:39:27 -0500

    Added input parsing

--- trapezoidal_map.py
+++ trapezoidal_map.py
@@ -4,10 +4,28 @@
 
     Description: Trapezoidal map construction and planar point location implementation
 """
+import sys
+
+def parseInput(filename):
+    print("Parsing!")
+    with open(filename) as f:
+        num_lines = int(f.readline().rstrip())
+        vals = [int(s) for s in str.split(f.readline().rstrip(), ' ')]
+        bound_box = [[vals[0], vals[1]],[vals[2], vals[3]]]
+        lines = []
+        for line in f:
+            if len(line) > 2:
+                vals = [int(s) for s in str.split(line.rstrip(), " ")]
+                lines.append( [[vals[0], vals[1]],[vals[2], vals[3]]] )
+
+    print(str(num_lines) + " in " + str(bound_box))
+    print(lines)
+
+    return num_lines, bound_box, lines
 
 def main():
-    print("Got to start somewhere")
-
+    print("Got to start somewhere, how about " + sys.argv[1])
+    parseInput(sys.argv[1])
 
 if __name__ == "__main__":
     main()
